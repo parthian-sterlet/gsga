9d1a36676b97fa7c88dfd5530c12ab46fc54847e
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2025-05-14 13:49:39 +0700

    Update build.sh

--- run/build.sh
+++ run/build.sh
@@ -1,6 +1,6 @@
 chmod a+x order
-chmod a+x improvement
-chmod a+x degradation
+chmod a+x improve
+chmod a+x destroy
 
 g++ -o genosensor_seq_order_ga.exe ../src/genosensor_seq_order_ga.cpp
 g++ -o genosensor_seq_ga.exe ../src/genosensor_seq_ga.cpp
