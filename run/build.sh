a5feb51561c7eeb859d101c79bc19825f20b204b
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2025-05-14 12:37:25 +0700

    Create build.sh

---
+++ run/build.sh
@@ -0,0 +1,6 @@
+chmod a+x order
+chmod a+x improvement
+chmod a+x degradation
+
+g++ -o genosensor_seq_order_ga.exe ../src/genosensor_seq_order_ga.cpp
+g++ -o genosensor_seq_ga.exe ../src/genosensor_seq_ga.cpp
